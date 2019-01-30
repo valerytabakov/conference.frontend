655debadee219e688acd086a84547f827030ba72
Valerii Tabakov <https://valery_tabakov@bitbucket.org>
2019-01-30 16:37:25 +0200

    start commit

---
+++ src/apiConfig.js
@@ -0,0 +1,7 @@
+//export const apiUrl = 'http://conference.backend';
+export const apiUrl = 'https://relaxed-morse-3844ae.netlify.com/';
+
+export const apiRoutes = {
+    news: '/news',
+    users: '/users',
+};
